32b738c1b10507d85af64a7803076cc309901bf5
Gabriel Massana <gabrielmassana@gmail.com>
2016-03-27 00:45:36 +0000

    some more tests

--- Gojimo-ObjCTests/Parsers/Qualifications/GOJQualificationsParserTests.m
+++ Gojimo-ObjCTests/Parsers/Qualifications/GOJQualificationsParserTests.m
@@ -19,7 +19,10 @@
 @property (nonatomic, strong) GOJQualificationsParser *parser;
 
 @property (nonatomic, strong) NSArray *qualificationsJSON;
+@property (nonatomic, strong) NSArray *qualificationsEmptyJSON;
+
 @property (nonatomic, strong) NSDictionary *qualificationJSON;
+@property (nonatomic, strong) NSDictionary *qualificationUpdatedJSON;
 @property (nonatomic, strong) NSDictionary *qualificationEmptyJSON;
 @property (nonatomic, strong) NSDictionary *qualificationOnlyIDJSON;
 
@@ -32,6 +35,7 @@
 @property (nonatomic, strong) NSString *qualificationLink;
 @property (nonatomic, strong) NSString *qualificationName;
 @property (nonatomic, strong) NSString *subjectId;
+@property (nonatomic, strong) NSString *qualificationNameUpdated;
 
 @end
 
@@ -54,13 +58,13 @@
     self.qualificationLink = @"/api/v4/qualifications/04ec172d-0c8f-4c09-94b4-d797b9481171";
     self.qualificationName = @"ASVAB";
     self.subjectId = @"91a1b8dc-1021-4ddd-a498-8d4a3f2dadf6";
-    
+    self.qualificationNameUpdated = @"ABCDEFG";
+
     self.qualificationOnlyIDJSON = @{
                                      @"id" : self.qualificationId,
                                      };
     
     self.qualificationEmptyJSON = @{
-                                    
                                     };
     self.qualificationJSON = @{
                                @"country" :     @{
@@ -75,27 +79,88 @@
                                @"link" : self.qualificationLink,
                                @"name" : self.qualificationName,
                                @"subjects" :     @[
-                                                       @{
-                                                           @"colour" : @"<null>",
-                                                           @"id" : self.subjectId,
-                                                           @"link" : @"/api/v4/subjects/91a1b8dc-1021-4ddd-a498-8d4a3f2dadf6",
-                                                           @"title" : @"Music",
-                                                       },
-                                                       @{
-                                                           @"colour" : @"#F1F1F1",
-                                                           @"id" : @"79ab192a-9cd7-4d07-859b-6e8f4668ddfa",
-                                                           @"link" : @"/api/v4/subjects/79ab192a-9cd7-4d07-859b-6e8f4668ddfa",
-                                                           @"title" : @"Latin",
-                                                       }
+                                       @{
+                                           @"colour" : @"<null>",
+                                           @"id" : self.subjectId,
+                                           @"link" : @"/api/v4/subjects/91a1b8dc-1021-4ddd-a498-8d4a3f2dadf6",
+                                           @"title" : @"Music",
+                                           },
+                                       @{
+                                           @"colour" : @"#F1F1F1",
+                                           @"id" : @"79ab192a-9cd7-4d07-859b-6e8f4668ddfa",
+                                           @"link" : @"/api/v4/subjects/79ab192a-9cd7-4d07-859b-6e8f4668ddfa",
+                                           @"title" : @"Latin",
+                                           }
                                        ],
                                };
-
+    
+    self.qualificationUpdatedJSON = @{
+                                      @"country" :     @{
+                                              @"code" : self.countryCode,
+                                              @"created_at" : self.countryCreated_at,
+                                              @"link" : self.countryLink,
+                                              @"name" : self.countryName,
+                                              @"updated_at" : self.countryOpdated_at,
+                                              },
+                                      @"default_products" :     @[],
+                                      @"id" : self.qualificationId,
+                                      @"link" : self.qualificationLink,
+                                      @"name" : self.qualificationNameUpdated,
+                                      @"subjects" :     @[
+                                              @{
+                                                  @"colour" : @"<null>",
+                                                  @"id" : self.subjectId,
+                                                  @"link" : @"/api/v4/subjects/91a1b8dc-1021-4ddd-a498-8d4a3f2dadf6",
+                                                  @"title" : @"Music",
+                                                  },
+                                              @{
+                                                  @"colour" : @"#F1F1F1",
+                                                  @"id" : @"79ab192a-9cd7-4d07-859b-6e8f4668ddfa",
+                                                  @"link" : @"/api/v4/subjects/79ab192a-9cd7-4d07-859b-6e8f4668ddfa",
+                                                  @"title" : @"Latin",
+                                                  }
+                                              ],
+                                      };
+    
+    self.qualificationsEmptyJSON = @[
+                                     ];
+    
+    self.qualificationsJSON = @[
+                                self.qualificationJSON,
+                                @{
+                                    @"country" :     @{
+                                            @"code" : self.countryCode,
+                                            @"created_at" : self.countryCreated_at,
+                                            @"link" : self.countryLink,
+                                            @"name" : self.countryName,
+                                            @"updated_at" : self.countryOpdated_at,
+                                            },
+                                    @"default_products" :     @[],
+                                    @"id" : @"00000000-1111-2222-3333-444455556666",
+                                    @"link" : self.qualificationLink,
+                                    @"name" : self.qualificationName,
+                                    @"subjects" :     @[
+                                            @{
+                                                @"colour" : @"<null>",
+                                                @"id" : self.subjectId,
+                                                @"link" : @"/api/v4/subjects/91a1b8dc-1021-4ddd-a498-8d4a3f2dadf6",
+                                                @"title" : @"Music",
+                                                },
+                                            @{
+                                                @"colour" : @"#F1F1F1",
+                                                @"id" : @"79ab192a-9cd7-4d07-859b-6e8f4668ddfa",
+                                                @"link" : @"/api/v4/subjects/79ab192a-9cd7-4d07-859b-6e8f4668ddfa",
+                                                @"title" : @"Latin",
+                                                }
+                                            ],
+                                    }
+                                ];
 }
 
 - (void)tearDown
 {
     [[CDFCoreDataManager sharedInstance] reset];
-
+    
     self.parser = nil;
     
     self.countryCode = nil;
@@ -107,10 +172,14 @@
     self.qualificationLink = nil;
     self.qualificationName = nil;
     self.subjectId = nil;
+    self.qualificationNameUpdated = nil;
     
     self.qualificationOnlyIDJSON = nil;
     self.qualificationEmptyJSON = nil;
     self.qualificationJSON = nil;
+    
+    self.qualificationsEmptyJSON = nil;
+    self.qualificationsJSON = nil;
     
     [super tearDown];
 }
@@ -146,7 +215,7 @@
     insertedQualification.qualificationID = self.qualificationId;
     
     /*---------------------*/
-
+    
     GOJQualification *returnedQualification = [self.parser parseQualification:self.qualificationJSON];
     
     XCTAssertEqualObjects(insertedQualification.objectID, returnedQualification.objectID,  @"Should have returned the existing Qualification");
@@ -240,6 +309,73 @@
     GOJQualification *qualification = [self.parser parseQualification:self.qualificationOnlyIDJSON];
     
     XCTAssertNil(qualification.name, @"name was not set");
+}
+
+#pragma mark Updated
+
+- (void)test_parseQualification_nameUpdated
+{
+    [self.parser parseQualification:self.qualificationJSON];
+    GOJQualification *qualificationUpdated = [self.parser parseQualification:self.qualificationUpdatedJSON];
+
+    XCTAssertEqualObjects(qualificationUpdated.name, self.qualificationNameUpdated, @"name was not properly set. Was set to: %@ rather than: %@", qualificationUpdated.name, self.qualificationNameUpdated);
+}
+
+#pragma mark - Qualifications
+
+- (void)test_parseQualification_arrayReturned
+{
+    NSArray *qualifications = [self.parser parseQualifications:self.qualificationsJSON];
+    
+    XCTAssertNotNil(qualifications, @"A valid array object wasn't created");
+}
+
+- (void)test_parseQualification_arrayReturned2
+{
+    NSArray *qualifications = [self.parser parseQualifications:self.qualificationsEmptyJSON];
+    
+    XCTAssertNotNil(qualifications, @"A valid array object wasn't created");
+}
+
+- (void)test_parseQualification_arrayCount
+{
+    NSArray *qualifications = [self.parser parseQualifications:self.qualificationsJSON];
+    
+    XCTAssertEqualObjects(@(qualifications.count), @(self.qualificationsJSON.count), @"Parsed qualifications count is wrong");
+}
+
+- (void)test_parseQualification_arrayCountEmpty
+{
+    NSArray *qualifications = [self.parser parseQualifications:self.qualificationsEmptyJSON];
+    
+    XCTAssertEqualObjects(@(qualifications.count), @(self.qualificationsEmptyJSON.count), @"Parsed qualifications count is wrong");
+}
+
+- (void)test_parseQualification_uniqueObjects
+{
+    NSArray *qualifications = [self.parser parseQualifications:self.qualificationsJSON];
+    
+    XCTAssertNotEqualObjects(qualifications.firstObject, qualifications.lastObject, @"Parsed array without different objects");
+}
+
+- (void)test_parseQualification_arrayWithSameOrderFirstObject
+{
+    NSArray *qualifications = [self.parser parseQualifications:self.qualificationsJSON];
+    
+    GOJQualification *firstObjectParsed = qualifications.firstObject;
+    NSDictionary *firstObject = self.qualificationsJSON.firstObject;
+    
+    XCTAssertEqualObjects(firstObjectParsed.qualificationID, firstObject[@"id"], @"Should have the same id: %@", firstObjectParsed.qualificationID);
+}
+
+- (void)test_parseQualification_arrayWithSameOrderLastObject
+{
+    NSArray *qualifications = [self.parser parseQualifications:self.qualificationsJSON];
+    
+    GOJQualification *lastObjectParsed  = qualifications.lastObject;
+    NSDictionary *lastObject = self.qualificationsJSON.lastObject;
+    
+    XCTAssertEqualObjects(lastObjectParsed.qualificationID, lastObject[@"id"], @"Should have the same id: %@", lastObjectParsed.qualificationID);
 }
 
 @end
